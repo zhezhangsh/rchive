14263a2476d6b62fbe56457edc14ef8c803d1c12
zhezhangsh <zhangz@email.chop.edu>
2015-04-04 23:55:54 -0400

    Create load.r

---
+++ load.r
@@ -0,0 +1,12 @@
+# Load functions through GitHub repo
+
+RCHIVE_HOME<-"/zhangz/rchive";
+
+library(devtools);
+
+###
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/extra/HtmlUtilities.r");
+
+
+###
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapKegg.r");
