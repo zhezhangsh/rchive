2a6ea4a28d37eeffdcd4d1db4cb979a90be5c069
zhezhangsh <zhangz@email.chop.edu>
2015-04-07 01:24:36 -0400

    Update load.r

--- load.r
+++ load.r
@@ -10,4 +10,4 @@ source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/extra/Htm
 
 ###
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapKegg.r");
-source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/ParseBiosystems.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapBiosystems.r");
