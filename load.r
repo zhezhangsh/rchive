fa046d25492fb99cd9ab82df5fbc4e6759eb8e5f
zhezhangsh <zhangz@email.chop.edu>
2015-04-22 07:43:00 -0400

    Update load.r

--- load.r
+++ load.r
@@ -9,10 +9,11 @@ source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/extra/Htm
 
 
 ###
-source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapKegg.r");
-source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapBiosystems.r");
-source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/ParseEntrez.r");
-source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/variant/ParseDbSNP.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/assembly/MapChromosome.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/ParseGtf.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/MapGtf.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/ParseEntrez.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapKegg.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapBiosystems.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/variant/ParseDbSNP.r");
 
