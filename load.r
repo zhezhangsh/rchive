3df0b2084053ce44ab9e30006e58f6a407692c8a
zhezhangsh <zhangz@email.chop.edu>
2015-04-21 22:39:30 -0400

    Update load.r

--- load.r
+++ load.r
@@ -14,4 +14,5 @@ source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/ParseEntrez.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/variant/ParseDbSNP.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/ParseGtf.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/MapGtf.r");
 
