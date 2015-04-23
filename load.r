701976b252b306ee41956298e633da7fe8ab44ee
zhezhangsh <zhezhangsh@users.noreply.github.com>
2015-04-23 12:02:14 -0400

    Update load.r

--- load.r
+++ load.r
@@ -13,6 +13,7 @@ source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/assembly/
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/ParseGtf.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/MapGtf.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/ParseEntrez.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/CombineIntron.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapKegg.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapBiosystems.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/variant/ParseDbSNP.r");
