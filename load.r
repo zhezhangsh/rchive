ccad8475d3209acbdf82ca27798373b80d56a545
zhezhangsh <zhezhangsh@users.noreply.github.com>
2015-04-23 22:34:01 -0400

    Update load.r

--- load.r
+++ load.r
@@ -17,4 +17,5 @@ source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene/Comb
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapKegg.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/gene.set/MapBiosystems.r");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/variant/ParseDbSNP.r");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/literature/ParsePubtator.r);
 
