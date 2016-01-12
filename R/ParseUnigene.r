cc8f4777e34a77b0526ee15767c9cb55abe31900
Ubuntu <ubuntu@ip-172-31-22-178.ec2.internal>
2016-01-12 22:24:59 +0000

    parse unigene

--- R/ParseUnigene.r
+++ R/ParseUnigene.r
@@ -47,39 +47,45 @@ ParseUnigene<-function(ftp.file, species='human', download.new=FALSE,
   });
   rownames(tbl)<-ids;
   tbl<-data.frame(tbl, stringsAsFactors = FALSE);
+  saveRDS(tbl, file=paste(path, 'r', paste(species, '_genes.rds', sep=''), sep='/')); 
+  out<-list(annotation=tbl); 
   
   # Tissue specific expression
   cat('Create tissue expression table ...\n');
   expr<-val[fld=='EXPRESS'];
-  expr<-strsplit(expr, '\\|'); 
-  expr.ind<-rep(names(expr), sapply(expr, length)); 
-  expr.val<-unlist(expr, use.names=FALSE);
-  expr.val<-stringr::str_trim(expr.val); 
-  expr.cnm<-sort(unique(expr.val));
-  expr.tbl<-matrix(0, nr=length(ids), nc=length(expr.cnm), dimnames=list(names(ids), expr.cnm)); 
-  for (i in 1:length(expr.cnm)) {
-    j<-unique(expr.ind[expr.val==expr.cnm[i]]); 
-    expr.tbl[j, i]<-1;
-  };
-  rownames(expr.tbl)<-ids; 
-  
+  if (length(expr) > 0) {
+    expr<-strsplit(expr, '\\|'); 
+    expr.ind<-rep(names(expr), sapply(expr, length)); 
+    expr.val<-unlist(expr, use.names=FALSE);
+    expr.val<-stringr::str_trim(expr.val); 
+    expr.cnm<-sort(unique(expr.val));
+    expr.tbl<-matrix(0, nr=length(ids), nc=length(expr.cnm), dimnames=list(names(ids), expr.cnm)); 
+    for (i in 1:length(expr.cnm)) {
+      j<-unique(expr.ind[expr.val==expr.cnm[i]]); 
+      expr.tbl[j, i]<-1;
+    };
+    rownames(exprd.tbl)<-ids;
+    saveRDS(expr.tbl, file=paste(path, 'r', paste(species, '_expression.rds', sep=''), sep='/')); 
+    out$expression<-expr.tbl;
+  }
+ 
   # Protein similarity to other species
   cat('Creating gene homolog table ...\n'); 
   prt<-val[fld=='PROTSIM']; 
-  prt<-strsplit(prt, '; '); 
-  prt<-do.call('rbind', prt); 
-  prt.cnm<-c('ORG', 'PROTGI', 'PROTID', 'PCT', 'ALN'); 
-  for (i in 1:ncol(prt)) prt[, i]<-sub(paste(prt.cnm[i], '=', sep=''), '', prt[, i]); 
-  tbl.prt<-cbind(ids[rownames(prt)], prt); 
-  rownames(tbl.prt)<-1:nrow(tbl.prt);
-  tbl.prt<-data.frame(tbl.prt, stringsAsFactors = FALSE); 
-  names(tbl.prt)<-c('ID', prt.cnm);
-  tbl.prt$PCT<-as.numeric(tbl.prt$PCT); 
-  tbl.prt$ALN<-as.numeric(tbl.prt$ALN); 
+  if (length(prt) > 0) {
+    prt<-strsplit(prt, '; '); 
+    prt<-do.call('rbind', prt); 
+    prt.cnm<-c('ORG', 'PROTGI', 'PROTID', 'PCT', 'ALN'); 
+    for (i in 1:ncol(prt)) prt[, i]<-sub(paste(prt.cnm[i], '=', sep=''), '', prt[, i]); 
+    tbl.prt<-cbind(ids[rownames(prt)], prt); 
+    rownames(tbl.prt)<-1:nrow(tbl.prt);
+    tbl.prt<-data.frame(tbl.prt, stringsAsFactors = FALSE); 
+    names(tbl.prt)<-c('ID', prt.cnm);
+    tbl.prt$PCT<-as.numeric(tbl.prt$PCT); 
+    tbl.prt$ALN<-as.numeric(tbl.prt$ALN); 
+    saveRDS(tbl.prt, file=paste(path, 'r', paste(species, '_homolog.rds', sep=''), sep='/')); 
+    out$homolog=tbl.prt;
+  }
   
-  saveRDS(tbl, file=paste(path, 'r', paste(species, '_genes.rds', sep=''), sep='/')); 
-  saveRDS(expr.tbl, file=paste(path, 'r', paste(species, '_expression.rds', sep=''), sep='/')); 
-  saveRDS(tbl.prt, file=paste(path, 'r', paste(species, '_homolog.rds', sep=''), sep='/')); 
-  
-  list(annotation=tbl, expression=expr.tbl, homolog=tbl.prt);
+  out; 
 }
