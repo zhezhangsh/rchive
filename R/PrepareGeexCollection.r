aa4b9e5c7be03245bfa87e917e6a4621e1e593f9
Ubuntu <ubuntu@ip-172-31-26-92.ec2.internal>
2017-01-08 15:34:45 +0000

    geex colleciton

--- R/PrepareGeexCollection.r
+++ R/PrepareGeexCollection.r
@@ -176,10 +176,13 @@ PrepareGeexCollection<-function(path.coll,
   
   gex.comb<-lapply(gex.comb, function(g) as.matrix(round(g, 2)));
   
+  fns <- paste(path.coll, '/gex_ff', 1:length(gex.comb), sep=''); 
+  for (i in 1:length(fns)) gex.comb[[i]] <- as.ff(gex.comb[[i]], fns[i]);
+  
   saveRDS(gex.comb, file=paste(path.coll, 'gex_combined.rds', sep='/'));
   saveRDS(anno, file=paste(path.coll, 'gene.rds', sep='/'));
   saveRDS(browse.tbl, file=paste(path.coll, 'browse_table.rds', sep='/'));
   saveRDS(mapping, file=paste(path.coll, 'mapping.rds', sep='/'));
   
-  mapping;
+  # mapping;
 }
