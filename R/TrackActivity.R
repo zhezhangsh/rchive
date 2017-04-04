a2c5606dcf74a6112b39ebf059395ca105f23c02
root <root@ip-172-31-26-92.ec2.internal>
2017-04-04 14:32:30 +0000

    update

--- R/TrackActivity.R
+++ R/TrackActivity.R
@@ -1,3 +1,9 @@
-TrackActivity <- function(session) {
+TrackActivity <- function(session, action = '', data = NULL, dir = '.') {
+  cdata <- session$clientData; 
+  ddata <- lapply(names(cdata), function(name) cdata[[name]]);
+  names(ddata) <- names(cdata);
   
+  fn <- paste(dir, '/', as.integer(Sys.time()), '_', session$token, '_', session$token, '.rds', sep='');
+  
+  saveRDS(list(clientData = ddata, action = action, data = data), fn); 
 }
